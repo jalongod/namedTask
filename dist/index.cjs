8309b09cd652a2f6aecea39ec46d912a41964708
张成荣 <zhangchengrong1@haier.com>
2023-04-17 11:21:19 +0800

    daily assets publish

--- dist/index.cjs
+++ dist/index.cjs
@@ -27,70 +27,31 @@ function __awaiter(thisArg, _arguments, P, generator) {
     });
 }
 
-function __generator(thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-}
-
 globalThis.__NAMED_TASK__ = globalThis.__NAMED_TASK__ || {};
-var globalTaskManager = globalThis.__NAMED_TASK__;
+const globalTaskManager = globalThis.__NAMED_TASK__;
 globalTaskManager.taskCache = globalTaskManager.taskCache || {};
 globalTaskManager.eventBus = globalTaskManager.eventBus || new events.EventEmitter();
-var taskCache = globalTaskManager.taskCache;
-var eventBus = globalTaskManager.eventBus;
+const taskCache = globalTaskManager.taskCache;
+const eventBus = globalTaskManager.eventBus;
 function getEventName(taskName) {
   return "NAMED_TASK_" + taskName;
 }
 function namedTask(name, task, args) {
-  return __awaiter(this, void 0, void 0, function() {
-    var eventName, ret, result;
-    return __generator(this, function(_a) {
-      switch (_a.label) {
-        case 0:
-          eventName = getEventName(name);
-          if (!taskCache[name])
-            return [3, 2];
-          return [4, new Promise(function(resolve) {
-            eventBus.on(eventName, function(result2) {
-              resolve(result2);
-            });
-          })];
-        case 1:
-          ret = _a.sent();
-          return [2, ret];
-        case 2:
-          taskCache[name] = task;
-          return [4, task(args)];
-        case 3:
-          result = _a.sent();
-          eventBus.emit(eventName, result);
-          delete taskCache[name];
-          return [2, result];
-      }
-    });
+  return __awaiter(this, void 0, void 0, function* () {
+    const eventName = getEventName(name);
+    if (taskCache[name]) {
+      const ret = yield new Promise((resolve) => {
+        eventBus.on(eventName, (result2) => {
+          resolve(result2);
+        });
+      });
+      return ret;
+    }
+    taskCache[name] = task;
+    const result = yield task(args);
+    eventBus.emit(eventName, result);
+    delete taskCache[name];
+    return result;
   });
 }
 
